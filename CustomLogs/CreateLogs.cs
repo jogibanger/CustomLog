0e6672cbd21bd7eb6365d530eeca45fb4376df1f
Joginder Singh <joginder.banger19@gmail.com>
2018-05-16 17:45:36 +0530

    Custom Log
    Custom Log..

---
+++ CustomLogs/CreateLogs.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLogs
+{
+   
+
+   public class CreateLogs 
+    {
+       string TableName = string.Empty;
+       string objConnection = string.Empty;
+       /// <summary>
+       /// Please enter the Table Name.
+       /// Please Enter the ConnectionString value.
+       /// </summary>
+       /// <param name="tableName"></param>
+       /// <param name="ConnectionString"></param>
+       public CreateLogs(string tableName, string ConnectionString)
+       {
+           TableName = tableName;
+           objConnection = ConnectionString;
+       }
+       public CreateLogs()
+       {
+           throw new NotImplementedException("Table Name Must be Requireds");
+       }
+     
+
+        public void CreateDatabaseLogs(object obj)
+        {
+            string IsTableCreated = string.Empty;
+            Dictionary<string, string> ObjDictionary = new Dictionary<string, string>();
+
+            var commandStr = "select name from sysobjects where name = '"+TableName+"'";
+
+            using (SqlConnection connection = new SqlConnection(objConnection))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(commandStr))
+                {
+                    command.Connection = connection;
+                    IsTableCreated = Convert.ToString( command.ExecuteScalar());
+                }
+
+            }
+           
+            string ClassName = obj.GetType().BaseType.Name;
+            Type myClassType = obj.GetType();
+            PropertyInfo[] properties = myClassType.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                ObjDictionary.Add(property.Name,Convert.ToString( property.GetValue(obj, null)));
+                
+                //Console.WriteLine("Name: " + property.Name + ", Value: " + property.GetValue(obj, null));
+            }
+            bool IsNewTableCreated = false;
+            if(string.IsNullOrEmpty(IsTableCreated))
+            {
+                StringBuilder ObjCreateTable = new StringBuilder();
+                ObjCreateTable.Append("CREATE TABLE "+TableName+" (");
+                ObjCreateTable.Append("" + TableName + "_ID int IDENTITY(1,1) NOT NULL, ");
+
+                foreach (var item in ObjDictionary)
+                {
+                    ObjCreateTable.Append("[" + item.Key + "] varchar(MAX) NULL, ");  
+                    
+                }
+
+                //",First_Name char(50),Last_Name char(50),Address char(50),City char(50),Country char(25),Birth_Date datetime)");
+                  ObjCreateTable.Append("[CreateDateTime] [datetime] NULL )");
+                  ObjCreateTable.Append("ALTER TABLE [dbo]." + TableName + " ADD  CONSTRAINT [" + TableName + " _CreateDateTime]  DEFAULT (getdate()) FOR [CreateDateTime]");
+                  string CreateTable = ObjCreateTable.ToString();
+
+                  using (SqlConnection connection = new SqlConnection(objConnection))
+                  {
+                      connection.Open();
+                      using (SqlCommand command = new SqlCommand(CreateTable))
+                      {
+                          command.Connection = connection;
+                         int IsTableCreateds= command.ExecuteNonQuery();
+                         IsNewTableCreated = true;
+                      }
+
+                  }
+            }
+            if(IsNewTableCreated==true || !string.IsNullOrEmpty(IsTableCreated) )
+            {
+                StringBuilder ObjCreateTable = new StringBuilder();
+                string ColumnName = string.Empty;
+                int counter1 = 0;
+                foreach (var item in ObjDictionary)
+                {
+                    counter1 += 1;
+                    if (counter1 == ObjDictionary.Count)
+                    {
+                        ColumnName += "[" + item.Key + "] ";
+                    }
+                    else
+                    {
+                        ColumnName += "[" + item.Key + "] ,";
+                       
+                    }
+                    
+                }
+
+                ObjCreateTable.Append("INSERT INTO [dbo]. " + TableName + "("+ColumnName+" ) VALUES (");
+                int counter = 0;
+                foreach (var item in ObjDictionary)
+                {
+                    counter += 1;
+                    if (counter == ObjDictionary.Count)
+                    {
+                        ObjCreateTable.Append("'" + item.Value + "' )");
+                    }
+                    else
+                    {
+                        ObjCreateTable.Append("'" + item.Value + "',");
+                    }
+
+                }
+                //ObjCreateTable.Append("'"+DateTime.Now.ToShortDateString()+"' )");
+                
+                using (SqlConnection connection = new SqlConnection(objConnection))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(ObjCreateTable.ToString()))
+                    {
+                        command.Connection = connection;
+                        int IsTableCreateds = command.ExecuteNonQuery();
+                        
+                    }
+
+                }
+
+
+            }
+
+
+           
+           
+        }
+
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+
+
+       
+    }
+}
---
+++ TestCustomLogs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCustomLogs
+{
+    public class NameOfValue
+    {
+        public string Name { get; set; }
+        public string Values { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = "Data Source=SV01;User ID=sa;Password=mpx123;Database=dummy";
+            CustomLogs.CreateLogs aa = new CustomLogs.CreateLogs("CustomLogs",ConnectionString);
+            NameOfValue obj=new NameOfValue();
+            obj.Name = "Have a nice day";
+            obj.Values = "1";
+            aa.CreateDatabaseLogs(obj);
+        }
+    }
+}
